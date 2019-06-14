7d1f9fa01e7dbdecb0a350139c79fad0c85f499f
Felipe <felipecaggi@gmail.com>
2019-06-14 02:29:01 -0400

    Criação de classe para ler dados e criar um dicionário de atributos

---
+++ datasets/car_evaluation/car_script.py
@@ -0,0 +1,15 @@
+from datasets.DataFile import DataFile
+
+if __name__ == '__main__':
+
+    # Read file and capture data
+    data_file = DataFile('./car.data')
+
+    # Raw data
+    raw_data = data_file.raw_data
+
+    # Dictionary containing attributes and their values
+    attribute_dictionary = data_file.attribute_dictionary
+
+    # Data processing based on the attribute dictionary
+    ready_data = data_file.ready_data
