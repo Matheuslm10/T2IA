29b1a6333954698662d853041fe9401c8c761f25
Matheuslm10 <matheus_lm10@hotmail.com>
2019-06-14 22:47:52 -0400

    percorrendo as 5 combinacoes de parametros do KNN

---
+++ datasets/GridSearcher.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import StratifiedKFold
+
+class GridSearcher:
+
+    def findBestParametersForKNN(X, y, skf):
+        k_range = list(range(1, 3))
+        weights_options = ['uniform', 'distance']
+        algorithm_options = ['auto', 'ball_tree', 'kd_tree', 'brute']
+        leaf_size_range = list(range(1, 2))
+
+        param_grid = dict(n_neighbors=k_range, weights=weights_options, algorithm=algorithm_options, leaf_size=leaf_size_range)
+        grid = GridSearchCV(KNeighborsClassifier(), param_grid, cv=skf, scoring='accuracy', return_train_score=False)
+        grid.fit(X, y)
+        resultados = pd.DataFrame(grid.cv_results_)[['mean_test_score', 'std_test_score', 'params']]
+        resultados_ordenados = resultados.sort_values('mean_test_score', ascending=False)
+
+        print('MELHOR ACURARICA: ', grid.best_score_)
+        print('MELHOR PARÂMETRO: ', grid.best_params_)
+        print('MELHOR ESTIMADOR: ', grid.best_estimator_)
+        combinacoesAlvo = resultados_ordenados.iloc[:5, 0:3]
+
+        return resultados_ordenados, combinacoesAlvo
--- datasets/iris/iris_script.py
+++ datasets/iris/iris_script.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from datasets.DataNormalizer import DataNormalizer as normalizer
+from datasets.GridSearcher import GridSearcher as gridsearcher
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
@@ -9,52 +10,41 @@ from sklearn import tree
 
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import GridSearchCV
-
-
-def findBestParametersForKNN(modelo, X, y, sfk):
-    k_range = list(range(1, 3))
-    weights_options = ['uniform', 'distance']
-    algorithm_options = ['auto', 'ball_tree', 'kd_tree', 'brute']
-    leaf_size_range = list(range(1, 3))
-
-    param_grid = dict(n_neighbors=k_range, weights=weights_options, algorithm=algorithm_options, leaf_size=leaf_size_range)
-    grid = GridSearchCV(modelo, param_grid, cv=skf, scoring='accuracy', return_train_score=False)
-    grid.fit(X, y)
-    resultados = pd.DataFrame(grid.cv_results_)[['mean_test_score', 'std_test_score', 'params']]
-    resultados_ordenados = resultados.sort_values('mean_test_score')
-
-
-    print('MELHOR ACURARICA: ', grid.best_score_)
-    print('MELHOR PARÂMETRO: ', grid.best_params_)
-    print('MELHOR ESTIMADOR: ', grid.best_estimator_)
-    combinacoesAlvo = resultados_ordenados.loc['mean_test_score']
-    return resultados_ordenados, combinacoesAlvo
 
 
 def calcularMediaAcuracia(nome, modelo):
     skf = StratifiedKFold(n_splits=10, shuffle=False)
     acuraciaPorFold = cross_val_score(modelo, X, y, cv=skf)
-    print(nome)
-    # print("Acurácias:", scores)
+    print(modelo)
+    print("Acurácias:", acuraciaPorFold)
     print("Acurácia (Média):", np.mean(acuraciaPorFold))
     print()
 
 
 if __name__ == '__main__':
-    dados = normalizer('./iris.data').ready_data
 
+    dados = normalizer('./iris.data').ready_data
 
     X = dados[:, 0:4]
     y = dados[:, [4]]
     y.shape = (150,)
 
-
+    modelo = KNeighborsClassifier()
     skf = StratifiedKFold(n_splits=10, shuffle=False)
 
-    modelo = KNeighborsClassifier()
+    resultados, combinacoesAlvo = gridsearcher.findBestParametersForKNN(X, y, skf)
 
-    resultados, combinacoesAlvo = findBestParametersForKNN(modelo, X, y, skf)
+    # PERCORRE AS 5 COMBINAÇÕES
+    for row in range(1,combinacoesAlvo.shape[0]+1):
+        print(row)
+        dicionario = combinacoesAlvo.iloc[(row-1):(row), 2:3]['params'].values[0]
+        algorithm = dicionario.get('algorithm')
+        leaf_size = dicionario.get('leaf_size')
+        n_neighbors = dicionario.get('n_neighbors')
+        weights = dicionario.get('weights')
+        modelo = KNeighborsClassifier(algorithm=algorithm, leaf_size=leaf_size, n_neighbors=n_neighbors, weights=weights)
+
+
 
     # calcularMediaAcuracia("REGRESSAO LOGISTICA:", LogisticRegression(solver='lbfgs', max_iter=1000, multi_class='multinomial'))
 
